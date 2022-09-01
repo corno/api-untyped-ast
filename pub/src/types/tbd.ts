77b86cd7fccd3174c56a64074435181ad38fc7f5
corno <corno.astn.a@schraverus.com>
2022-09-01 23:13:34 +0200

    .

--- pub/src/types/tbd.ts
+++ pub/src/types/tbd.ts
@@ -1,11 +1,15 @@
 import * as pt from "pareto-core-types"
 
+export type TLocation = {
+    readonly "line": number
+    readonly "column": number
+}
+
 export type TUntypedNode = {
     readonly "kindName": string
     readonly "value": string
-    readonly "location": {
-        line: number
-        column: number
+    readonly "details": {
+        readonly "location": TLocation
     }
     readonly "children": pt.Array<TUntypedNode>
 }
